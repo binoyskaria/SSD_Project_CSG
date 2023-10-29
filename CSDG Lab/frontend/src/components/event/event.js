aadaaef8998ba8994a6e531514d41bc269fd740d
Asritha Samala <asrithasamala771@gmail.com>
2023-10-29 21:18:02 +0530

    updatedevents

--- CSDG Lab/frontend/src/components/event/event.js
+++ CSDG Lab/frontend/src/components/event/event.js
@@ -5,7 +5,7 @@ import './event.css';
 const Event = () => {
   return (
     <div className="content">
-      <h1><Link to="/hackathon" className="hackathon-link">Hardware Accelerator Design Hackathon</Link></h1>
+      <h1 className="hackathon-title"><Link to="/hackathon" className="hackathon-link">Hardware Accelerator Design Hackathon</Link></h1>
       <p>April 30th to May 4th (Tentatively)</p>
     </div>
   );
